67c21e592763841b1af91696d638c61be1b1c050
Dulce Padrón <djhpadron246@gmail.com>
2023-11-13 23:41:02 -0600

    prueba

--- servicios_api.js
+++ servicios_api.js
@@ -10,11 +10,10 @@ const products = require('./products.json');
 //Para analizar solicitudes con carga útil en formato JSON
 app.use(express.json());
 
-// Configura CORS para permitir solo solicitudes
-
-  app.use(cors({
-    origin: 'https://bazaruniversal246.netlify.app', // Permitir solo solicitudes desde este origen (localhost)
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos HTTP permitidos
+// Configura CORS para permitir solo solicitudes desde http://127.0.0.1:5173
+app.use(cors({
+    origin: 'http://localhost:5173', // Permitir solo solicitudes desde este origen (localhost)
+    methods: 'GET,HEAD', // Métodos HTTP permitidos
     credentials: true, // Habilitar el envío de credenciales (por ejemplo, cookies)
   }));
   
